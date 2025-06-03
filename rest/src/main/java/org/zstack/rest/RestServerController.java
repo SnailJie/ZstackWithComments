8fbfa44c22d9bf7f31fa55ea49ff9b5844a7a23a
rj276016 <rj276016@antgroup.com>
2025-06-03 13:56:54 +0800

    fix

--- rest/src/main/java/org/zstack/rest/RestServerController.java
+++ rest/src/main/java/org/zstack/rest/RestServerController.java
@@ -9,10 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 import java.lang.reflect.InvocationTargetException;
-
 /**
  * Created by xing5 on 2016/12/7.
  */
@@ -29,7 +28,11 @@ public class RestServerController {
             }
     )
     public void api(HttpServletRequest request, HttpServletResponse response) throws IOException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Map<String,String> testInfo = new HashMap<>();
+        testInfo.put("A","good");
+        request.setAttribute("info",testInfo);
         server.handle(request, response);
-        Map<String,String> testInfl = new HashMap<>();
+
+
     }
 }
